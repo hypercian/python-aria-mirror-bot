6268a0a387e333f51a8e94231f2dcb7f81c875a9
hypercianstand <65367898+hypercianstand@users.noreply.github.com>
2020-06-01 08:57:05 +0430

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,7 @@
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=3
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
+export MAX_CONCURRENT_DOWNLOADS=20
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 8210 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+   --max-overall-upload-limit=1K --bt-stop-timeout=15552000 --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
